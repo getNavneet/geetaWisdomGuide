bd0eb8b0c5a2b991e87b904dfef265121a46ffdb
Navneet Kumar <navneetkumar.html@gmail.com>
2025-11-06 22:28:05 +0530

    working

--- src/useGeminiApi.js
+++ src/useGeminiApi.js
@@ -2,7 +2,6 @@ import { GoogleGenAI } from "@google/genai";
 
 const API_KEY = import.meta.env.VITE_GEMINI_API_KEY || '';
 // const genAI = new GoogleGenerativeAI(API_KEY);
-console.log("Using Gemini API Key:", API_KEY);
 const ai = new GoogleGenAI({
   apiKey: API_KEY
 });
